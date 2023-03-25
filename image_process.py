1cbe489e2b4a9d73aef3a93b13210aefa59a42e7
GuldenizBektas <gdenizbektass@gmail.com>
2023-03-25 20:10:27 +0300

    Add project 🌈

---
+++ image_process.py
@@ -0,0 +1,223 @@
+import os
+import json
+
+import numpy as np
+import itertools
+import matplotlib.pyplot as plt
+import numpy as np
+
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+
+import time
+
+import tensorflow as tf
+from tensorflow import keras
+from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing import image_dataset_from_directory
+
+# set the seed
+keras.utils.set_random_seed(42)
+
+# walk through each classes and get how many images are there
+def walk_through_dir(dir_path):
+    for dirpath, dirnames, filenames in os.walk(dir_path):
+        print(f"There are {len(dirnames)} directiories and {len(filenames)} images in {dirpath}.")
+
+def create_dataset(datasettype, image_path, subset_type, image_size, batch_size, split_size=0.2, seed=42):
+    """
+    This function will create Tensorflow datasets.
+    """
+    datasettype = image_dataset_from_directory(
+        image_path,
+        validation_split=split_size,
+        subset=subset_type,
+        seed=seed,
+        image_size=image_size,
+        batch_size=batch_size
+    )
+    return datasettype
+
+def display_image(dataset, class_names, img_count=9):
+    plt.figure(figsize=(10,10))
+    for images, labels in dataset.take(1):
+        for i in range(img_count):
+            ax = plt.subplot(int(img_count/3), int(img_count/3), i+1)
+            plt.imshow(images[i].numpy().astype("uint8"))
+            plt.title(class_names[labels[i]])
+            plt.axis("off")
+    plt.show()
+
+def display_augmented_images(dataset, augmentation, img_count=9):
+    plt.figure(figsize=(10,10))
+    for images, labels in dataset.take(1):
+        for i in range(img_count):
+            augmente_image = augmentation(images[i])
+            ax = plt.subplot(int(img_count/3), int(img_count/3), i+1)
+            plt.imshow(augmente_image.numpy().astype("uint8"))
+            plt.axis("off")
+    plt.show()
+
+# Create a function to import an image and resize it to be able to be used with our model
+def load_and_prep_image(filename, img_shape=228, scale=True):
+  """
+  Reads in an image from filename, turns it into a tensor and reshapes into
+  (224, 224, 3).
+  Parameters
+  ----------
+  filename (str): string filename of target image
+  img_shape (int): size to resize target image to, default 224
+  scale (bool): whether to scale pixel values to range(0, 1), default True
+  """
+  # Read in the image
+  img = tf.io.read_file(filename)
+  # Decode it into a tensor
+  img = tf.image.decode_jpeg(img)
+  # Resize the image
+  img = tf.image.resize(img, [img_shape, img_shape])
+  if scale:
+    # Rescale the image (get all values between 0 and 1)
+    return img/255.
+  else:
+    return img
+
+def pred_and_plot(model, filename, class_names, scale=False):
+  """
+  Imports an image located at filename, makes a prediction on it with
+  a trained model and plots the image with the predicted class as the title.
+  """
+  # Import the target image and preprocess it
+  img = load_and_prep_image(filename, scale=scale)
+  
+  # Make a prediction
+  pred = model.predict(tf.expand_dims(img, axis=0))
+  #print(pred)
+  pred_class = class_names[np.argmax(pred[0])] # if more than one output, take the max
+  # Plot the image and predicted class
+  plt.imshow(img[0:].numpy().astype("uint8"))
+  plt.title(f"Prediction: {pred_class}")
+  plt.axis(False);
+  plt.savefig("prediction.png")
+
+# Our function needs a different name to sklearn's plot_confusion_matrix
+def make_confusion_matrix(y_true, y_pred, classes=None, figsize=(10, 10), text_size=15, norm=False, savefig=False): 
+  """Makes a labelled confusion matrix comparing predictions and ground truth labels.
+  If classes is passed, confusion matrix will be labelled, if not, integer class values
+  will be used.
+  Args:
+    y_true: Array of truth labels (must be same shape as y_pred).
+    y_pred: Array of predicted labels (must be same shape as y_true).
+    classes: Array of class labels (e.g. string form). If `None`, integer labels are used.
+    figsize: Size of output figure (default=(10, 10)).
+    text_size: Size of output figure text (default=15).
+    norm: normalize values or not (default=False).
+    savefig: save confusion matrix to file (default=False).
+  
+  Returns:
+    A labelled confusion matrix plot comparing y_true and y_pred.
+  Example usage:
+    make_confusion_matrix(y_true=test_labels, # ground truth test labels
+                          y_pred=y_preds, # predicted labels
+                          classes=class_names, # array of class label names
+                          figsize=(15, 15),
+                          text_size=10)
+  """  
+  # Create the confustion matrix
+  cm = confusion_matrix(y_true, y_pred)
+  cm_norm = cm.astype("float") / cm.sum(axis=1)[:, np.newaxis] # normalize it
+  n_classes = cm.shape[0] # find the number of classes we're dealing with
+
+  # Plot the figure and make it pretty
+  fig, ax = plt.subplots(figsize=figsize)
+  cax = ax.matshow(cm, cmap=plt.cm.Blues) # colors will represent how 'correct' a class is, darker == better
+  fig.colorbar(cax)
+
+  # Are there a list of classes?
+  if classes:
+    labels = classes
+  else:
+    labels = np.arange(cm.shape[0])
+  
+  # Label the axes
+  ax.set(title="Confusion Matrix",
+         xlabel="Predicted label",
+         ylabel="True label",
+         xticks=np.arange(n_classes), # create enough axis slots for each class
+         yticks=np.arange(n_classes), 
+         xticklabels=labels, # axes will labeled with class names (if they exist) or ints
+         yticklabels=labels)
+  
+  # Make x-axis labels appear on bottom
+  ax.xaxis.set_label_position("bottom")
+  ax.xaxis.tick_bottom()
+
+  # Set the threshold for different colors
+  threshold = (cm.max() + cm.min()) / 2.
+
+  # Plot the text on each cell
+  for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+    if norm:
+      plt.text(j, i, f"{cm[i, j]} ({cm_norm[i, j]*100:.1f}%)",
+              horizontalalignment="center",
+              color="white" if cm[i, j] > threshold else "black",
+              size=text_size)
+    else:
+      plt.text(j, i, f"{cm[i, j]}",
+              horizontalalignment="center",
+              color="white" if cm[i, j] > threshold else "black",
+              size=text_size)
+
+  # Save the figure to the current working directory
+  if savefig:
+    fig.savefig("confusion_matrix.png")
+
+def plot_loss_curves(history):
+  """"
+  Returns separate loss curves for training and validation metrics.
+  Args:
+    history: TensorFlow model History object (see: https://www.tensorflow.org/api_docs/python/tf/keras/callbacks/History)
+  """ 
+  loss = history.history['loss']
+  val_loss = history.history['val_loss']
+
+  accuracy = history.history['accuracy']
+  val_accuracy = history.history['val_accuracy']
+
+  epochs = range(len(history.history['loss']))
+
+  # Plot loss
+  plt.plot(epochs, loss, label='training_loss')
+  plt.plot(epochs, val_loss, label='val_loss')
+  plt.title('Loss')
+  plt.xlabel('Epochs')
+  plt.legend()
+
+  # Plot accuracy
+  plt.figure()
+  plt.plot(epochs, accuracy, label='training_accuracy')
+  plt.plot(epochs, val_accuracy, label='val_accuracy')
+  plt.title('Accuracy')
+  plt.xlabel('Epochs')
+  plt.legend()
+  plt.savefig("plot_loss_curve.png")
+
+# Function to evaluate: accuracy, precision, recall, f1-score
+def calculate_results(y_true, y_pred):
+      """
+      Calculates model accuracy, precision, recall and f1 score of a binary classification model.
+      Args:
+          y_true: true labels in the form of a 1D array
+          y_pred: predicted labels in the form of a 1D array
+      Returns a dictionary of accuracy, precision, recall, f1-score.
+      """
+      # Calculate model accuracy
+      model_accuracy = accuracy_score(y_true, y_pred)
+      # Calculate model precision, recall and f1 score using "weighted average
+      model_precision, model_recall, model_f1, _ = precision_recall_fscore_support(y_true, y_pred, average="weighted")
+      model_results = {"accuracy": model_accuracy,
+                      "precision": model_precision,
+                      "recall": model_recall,
+                      "f1": model_f1}
+      with open("results.json", "w") as outfile:
+        json.dump(model_results, outfile)
+      return model_results
---
+++ train.py
@@ -0,0 +1,125 @@
+from image_process import (create_dataset, display_augmented_images, display_image, 
+                           plot_loss_curves, make_confusion_matrix, pred_and_plot,
+                           calculate_results, load_and_prep_image)
+
+import numpy as np
+import os
+import time
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import Adam, RMSprop
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras import regularizers
+from tensorflow.keras import mixed_precision
+import matplotlib.pyplot as plt
+import matplotlib.image as mpmig
+
+# set the seed
+keras.utils.set_random_seed(42)
+
+# define image path
+IMAGE_PATH = "brain-tumor-mri-images-44-classes"
+
+# set some hyperparameters
+BATCH_SIZE=32
+IMAGE_WIDTH=228
+IMAGE_HEIGHT=228
+
+# create datasets
+train_ds = create_dataset("train",
+                image_path=IMAGE_PATH,  
+                subset_type="training", 
+                image_size=(IMAGE_HEIGHT, IMAGE_WIDTH),
+                batch_size=BATCH_SIZE,
+                split_size=.2,
+                seed=42)
+valid_ds = create_dataset("validation",
+                image_path=IMAGE_PATH,  
+                subset_type="validation", 
+                image_size=(IMAGE_HEIGHT, IMAGE_WIDTH),
+                batch_size=BATCH_SIZE,
+                split_size=.2,
+                seed=42
+)
+classes = train_ds.class_names
+valid_classes = valid_ds.class_names
+
+y_true = np.concatenate([y for x, y in valid_ds], axis=0)
+
+# add some normalization and data augmentation
+normalization_layer = tf.keras.layers.Rescaling(1./255)
+
+# apply norm and describing it as new dataset
+train_data = train_ds.map(lambda x, y: (normalization_layer(x), y), num_parallel_calls=tf.data.AUTOTUNE)
+valid_data = valid_ds.map(lambda x, y: (normalization_layer(x), y), num_parallel_calls=tf.data.AUTOTUNE)
+
+# apply prefetch to both dataset
+train_data = train_data.prefetch(tf.data.AUTOTUNE)
+valid_data = valid_data.prefetch(tf.data.AUTOTUNE)
+
+train_ds = train_ds.prefetch(tf.data.AUTOTUNE)
+valid_ds = valid_ds.prefetch(tf.data.AUTOTUNE)
+
+# augmentation
+# data_augmentation = Sequential([
+#     layers.RandomFlip("horizontal"),
+#     layers.RandomRotation(.5),
+#     layers.RandomZoom(.4)
+# ])
+
+#display_augmented_images(valid_ds, augmentation=data_augmentation, img_count=12)
+
+# create model
+inputs = keras.Input(shape=(IMAGE_HEIGHT, IMAGE_WIDTH, 3))
+#x = data_augmentation(inputs)
+
+x = Conv2D(128, kernel_size=3, activation="relu", padding="same")(inputs)
+x = MaxPooling2D(2)(x)
+x = Conv2D(128, kernel_size=3, activation="relu", padding="same")(x)
+x = layers.Dropout(.2)(x)
+x = Conv2D(64, kernel_size=3, activation="relu", padding="same")(x)
+x = MaxPooling2D(2)(x)
+
+x = Flatten(name="Flatten")(x)
+outputs = Dense(44, activation="softmax")(x)
+
+model = keras.Model(inputs=inputs, outputs=outputs)
+
+###### callbacks ########
+early_stopping = EarlyStopping(monitor="val_loss", patience=3)
+
+checkpoint_path = "checkpoints/cp.cpkt"
+model_checkpoint = ModelCheckpoint(checkpoint_path,
+                                    monitor="val_accuracy",
+                                    save_best_only=True,
+                                    save_weights_only=True,
+                                    verbose=1)
+
+mixed_precision.set_global_policy(policy="mixed_float16")
+print(mixed_precision.global_policy())
+
+# compile model
+model.compile(
+    optimizer=Adam(),
+    loss=tf.losses.SparseCategoricalCrossentropy(),
+    metrics=["accuracy"]
+)
+
+history_ds = model.fit(train_ds, validation_data=valid_ds,
+                       batch_size=BATCH_SIZE,
+                       epochs=50,
+                       callbacks=[early_stopping, model_checkpoint])
+
+y_pred = model.predict(valid_ds, verbose=1)
+y_pred = np.argmax(y_pred, axis=1)
+
+plot_loss_curves(history_ds)
+pred_and_plot(model, "Astrocitoma T2.jpeg", classes, scale=False)
+calculate_results(y_true, y_pred)
+make_confusion_matrix(y_true, y_pred, figsize=(20,20), text_size=20, classes=valid_classes, savefig=True)
